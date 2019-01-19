f4d7f1b56c40b0d2e1fa5277ed6504a516edd058
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-01-18 23:46:19 -0300

    dist available via postinstall

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import Authorize from "./decorators/authorize";
-import decorateWith from "./decorators/decorator";
-import OperationProcessor from "./operation-processor";
-import ResourceRegistry from "./resource-registry";
-export { OperationProcessor, ResourceRegistry, decorateWith, Authorize };
-export * from "./types";
