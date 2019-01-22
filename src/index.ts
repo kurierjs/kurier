be7e91b58aede4172b91dbef2604024cec033fc8
Maciej Kwaśniak <kmaciek17@gmail.com>
2019-01-22 13:02:14 +0100

    Export KnexProcessor

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import Authorize from "./decorators/authorize";
 import decorateWith from "./decorators/decorator";
+import KnexProcessor from "./knex-processor";
 import OperationProcessor from "./operation-processor";
 import ResourceRegistry from "./resource-registry";
 
 export {
   // Core objects
   OperationProcessor,
+  KnexProcessor,
   ResourceRegistry,
   // Decorators API
   decorateWith,
