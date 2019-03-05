5bbffb695f9f77f74b95a8e814f17833e7a2f89a
Ryan Tablada <ryan.tablada@gmail.com>
2019-03-05 13:39:10 -0600

    test: update dummy app with really bad relationship

--- tests/dummy/src/resources/user/processor.ts
+++ tests/dummy/src/resources/user/processor.ts
@@ -1,10 +1,49 @@
-import { KnexProcessor } from "../../jsonapi-ts";
+import { KnexProcessor, Operation, KnexRecord } from "../../jsonapi-ts";
 import User from "./resource";
+import { HasId } from "../../../../../src/processors/operation-processor";
 
 const timeout = (data, time) => new Promise(resolve => setTimeout(() => resolve(data), time));
 
 export default class UserProcessor extends KnexProcessor<User> {
   resourceClass = User;
+
+  async get(op: Operation): Promise<HasId[]> {
+    const { params, ref } = op;
+    const { id, type } = ref;
+    const tableName = this.typeToTableName(type);
+    const filters = params ? { id, ...(params.filter || {}) } : { id };
+    const resource = Object.create(this.resourceFor(type));
+    const fields = params ? { ...params.fields } : {};
+    const attributes = this.getColumns(
+      Object.keys(resource.__proto__.attributes),
+      fields,
+      type
+    );
+
+    let records: KnexRecord[] = await this.knex(tableName)
+      .leftOuterJoin('posts', 'posts.user_id', 'users.id')
+      .where(queryBuilder => this.filtersToKnex(queryBuilder, filters))
+      .select(...attributes, "users.id", 'posts.id as post.id', 'posts.title as post.title')
+      .modify(queryBuilder => this.optionsBuilder(queryBuilder, op));
+
+    // This is to extract the post stuff (this isn't 100% since left outer joins give multiple results per record if more than one post)
+    records = records.map(record => {
+      let r = {...record, posts: null };
+      delete r['post.title'];
+      delete r['post.id'];
+
+      if (record['post.id']) {
+        r.posts = [{
+          id: record['post.id'],
+          title: record['post.title'],
+        }];
+      }
+
+      return r
+    });
+
+    return records;
+  }
 
   attributes = {
     async friends(user: User) {
@@ -12,6 +51,25 @@ export default class UserProcessor extends KnexProcessor<User> {
         {name: 'Joel'},
         {name: 'Ryan'},
       ], 2000);
+    },
+
+    coolFactor(): number {
+      return 3;
+    }
+  }
+
+  relationships = {
+    async posts(this: UserProcessor, user: any) {
+      if (user.posts !== undefined) {
+        console.log('Loading Already Done!');
+        return user.posts;
+      }
+
+      console.log('Loading Async');
+
+      return await this.knex('posts')
+        .where({ user_id: user.id })
+        .select();
     }
   }
 }
--- tests/dummy/src/resources/user/resource.ts
+++ tests/dummy/src/resources/user/resource.ts
@@ -3,6 +3,11 @@ import { Resource } from "../../jsonapi-ts";
 export default class User extends Resource {
   static attributes = {
     email: String,
-    friends: Array
+    friends: Array,
+    coolFactor: Number
   }
+
+  static relationships: {
+    posts: ''
+  };
 }
