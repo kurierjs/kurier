2815e0413cb4eacc1edfba83b269a9a08a0b4646
Maciej Kwaśniak <kmaciek17@gmail.com>
2019-03-06 13:33:32 +0100

    Declare default relationships on resource init

--- src/resource.ts
+++ src/resource.ts
@@ -4,22 +4,29 @@ import { camelize } from "./utils/string";
 export default abstract class Resource {
   public type: string;
   public id?: string;
-  public attributes: ResourceTypeAttributes;
-  public relationships: ResourceTypeRelationships;
+  public attributes: ResourceTypeAttributes = {};
+  public relationships: ResourceTypeRelationships = {};
 
   constructor({
     id,
     attributes,
-    relationships
   }: {
     id?: string;
     attributes?: ResourceTypeAttributes;
-    relationships?: ResourceTypeRelationships;
   }) {
     this.type = camelize(this.constructor.name);
 
     this.id = id;
     this.attributes = attributes || {};
-    this.relationships = relationships || {};
+
+    Object.keys(this.constructor.relationships).forEach(relationshipKey => {
+      const relationship = this.constructor.relationships[relationshipKey];
+
+      if (relationship.kind === "belongsTo") {
+        this.relationships[relationshipKey] = { data: null };
+      } else if (relationship.kind === "hasMany") {
+        this.relationships[relationshipKey] = { data: [] };
+      }
+    });
   }
 }
