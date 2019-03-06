f68d72fc3061e08434d5bb1f60619b13279cf5c1
Maciej Kwaśniak <kmaciek17@gmail.com>
2019-03-06 13:37:21 +0100

    Allow to include hasMany and belongsTo relations

--- src/application.ts
+++ src/application.ts
@@ -1,6 +1,18 @@
+import * as flat from "array.prototype.flat";
+
 import OperationProcessor from "./processors/operation-processor";
 import Resource from "./resource";
+import {
+  JsonApiParams,
+  Operation,
+  OperationResponse,
+  ResourceConstructor
+} from "./types";
 import { camelize, classify, singularize } from "./utils/string";
+
+function toArray(item: any): any[] {
+  return (Array.isArray(item) ? item : [item]).filter(Boolean) as Resource[];
+}
 
 export default class Application {
   public namespace?: string;
@@ -28,15 +40,116 @@ export default class Application {
   async executeOperations(ops: Operation[]): Promise<OperationResponse[]> {
     return await this.createTransaction(
       ops.map(async op => {
-        const processor = this.processorFor(op);
-        const result = await processor.execute(op);
-        return this.buildOperationResponse(result, processor.flushIncludes());
+        const processor = this.processorFor(op.ref.type);
+        const data = await processor.execute(op);
+        const included = await this.resolveIncludes(op, data);
+
+        return this.buildOperationResponse(data, included);
       })
     );
   }
 
   async createTransaction(ops: Promise<OperationResponse>[]) {
     return await Promise.all(ops);
+  }
+
+  async resolveIncludes(
+    op: Operation,
+    data: Resource | Resource[] | void
+  ): Promise<Resource[]> {
+    const resourceType = op.ref.type;
+    const resources: Resource[] = toArray(data);
+
+    if (!resources.length) {
+      return [];
+    }
+
+    const resourcesIds = resources.map((r: Resource) => r.id);
+
+    const promises = (op.params.include || []).map(async include => {
+      // For now we do not support nested includes like `user.profiles`
+      const type = classify(singularize(include));
+      const processor = this.processorFor(type);
+      const resourceClass = this.resourceFor(resourceType);
+      const relationship = resourceClass.relationships[include] || { kind: "" };
+      const filterKey = this.relationshipFilterKey(
+        resourceType,
+        relationship.kind
+      );
+
+      const includeOp = {
+        op: "get",
+        ref: {
+          type
+        },
+        params: {
+          filter: {
+            [filterKey]: resourcesIds
+          }
+        } as JsonApiParams
+      } as Operation;
+
+      const includedResources = await processor.execute(includeOp);
+
+      toArray(includedResources).forEach(includedResource => {
+        // assign relationships for includedResource
+        Object.keys(includedResource.constructor.relationships).forEach(relationshipKey => {
+            const relationship =
+              includedResource.constructor.relationships[relationshipKey];
+
+            if (relationship.kind === "belongsTo") {
+              includedResource.relationships[relationshipKey] = {
+                data: {
+                  id: includedResource.attributes[filterKey],
+                  type: relationship.type
+                }
+              };
+            } else if (relationship.kind === "hasMany") {
+              if (includedResource.relationships[relationshipKey].data) {
+                includedResource.relationships[relationshipKey].data = [];
+              }
+
+              includedResource.relationships[relationshipKey].data.push({
+                id: includedResource.id,
+                type: relationship.type
+              });
+            }
+          }
+        );
+
+        // assign included relationship for resource
+        if (relationship.kind === "belongsTo") {
+          const resource = resources.find(
+            r => r.id === includedResource.id
+          ) as Resource;
+
+          resource.relationships[relationship.name].data = {
+            id: includedResource.id,
+            type: includedResource.type
+          };
+        } else if (relationship.kind === "hasMany") {
+          const resource = resources.find(
+            r => r.id === includedResource.attributes[filterKey]
+          ) as Resource;
+
+          resource.relationships[relationship.name] = {
+            data: [{ id: includedResource.id, type: includedResource.type }]
+          };
+        }
+      });
+
+      return includedResources;
+    });
+
+    return flat(await Promise.all(promises));
+  }
+
+  relationshipFilterKey(type: string, relationshipKind: string): string {
+    if (relationshipKind === "hasMany") {
+      return `${camelize(singularize(type))}Id`;
+    }
+
+    return "id";
   }
 
   resourceFor(type: string = ""): ResourceConstructor {
@@ -54,11 +167,11 @@ export default class Application {
 
   buildOperationResponse(
     data: Resource | Resource[] | void,
-    included: Resource[] | void
+    included: Resource[]
   ): OperationResponse {
     return {
-      data: data || null,
-      included: included || null
+      included,
+      data: data || null
     };
   }
 }
