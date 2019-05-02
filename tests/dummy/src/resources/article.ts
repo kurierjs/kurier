8bf2c58d179a4421e0070f0ddd53513d6472f920
Santiago Persico <santiagopersico@gmail.com>
2019-05-02 12:25:18 -0300

    another fix test schema definition

--- tests/dummy/src/resources/article.ts
+++ tests/dummy/src/resources/article.ts
@@ -18,12 +18,12 @@ export default class Article extends Resource {
       votes: {
         type: () => Vote,
         hasMany: true,
-        foreignKeyName: 'article_id',
-        comments: {
-          type: () => Comment,
-          hasMany: true,
-          foreignKeyName: 'author_id'
-        }
+        foreignKeyName: 'article_id'
+      },
+      comments: {
+        type: () => Comment,
+        hasMany: true,
+        foreignKeyName: 'author_id'
       }
     }
   }
