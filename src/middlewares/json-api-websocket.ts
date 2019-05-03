edf4c3fc7ffc916d8cfc99f8e2c9297e37712e7f
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-05-03 16:15:39 +0200

    implemented WS authentication via meta.token

--- src/middlewares/json-api-websocket.ts
+++ src/middlewares/json-api-websocket.ts
@@ -1,26 +1,48 @@
 import { Server } from "ws";
+
 import Application from "../application";
 import { JsonApiDocument } from "../types";
+import ApplicationInstance from "../application-instance";
 
 export default function jsonApiWebSocket(
   websocketServer: Server,
   app: Application
 ) {
   websocketServer.on("connection", connection => {
-    connection.on("message", async message => {
-      if (!message) {
-        return;
+    connection.on("message", async (message: Buffer) => {
+      try {
+        const appInstance = new ApplicationInstance(app);
+
+        if (!message) {
+          return;
+        }
+
+        const { meta, operations } = JSON.parse(
+          message.toString()
+        ) as JsonApiDocument;
+
+        // Get user.
+        if (meta && meta.token) {
+          appInstance.user = await appInstance.getUserFromToken(
+            meta.token as string
+          );
+        }
+
+        // Execute and reply.
+        const response = await appInstance.app.executeOperations(operations);
+
+        connection.send(
+          JSON.stringify({
+            operations: response
+          })
+        );
+      } catch (e) {
+        connection.send(
+          JSON.stringify({
+            errors: [e]
+          })
+        );
       }
-
-      const { operations } = JSON.parse(message.toString()) as JsonApiDocument;
-
-      const response = await app.executeOperations(operations);
-
-      connection.send(
-        JSON.stringify({
-          operations: response
-        })
-      );
     });
   });
 }
