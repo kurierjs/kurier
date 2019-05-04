8445964849c011a4cf1e9ccc50acc22623adcac8
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-05-04 16:18:11 +0200

    implemented User, Session resources

---
+++ src/resources/session.ts
@@ -0,0 +1,23 @@
+import Resource from "../resource";
+import User from "./user";
+import Password from "../attribute-types/password";
+
+export default class Session extends Resource {
+  public static get type() {
+    return "session";
+  }
+
+  public static schema = {
+    attributes: {
+      token: String,
+      username: String,
+      password: Password
+    },
+    relationships: {
+      user: {
+        type: () => User,
+        belongsTo: true
+      }
+    }
+  };
+}
---
+++ src/resources/user.ts
@@ -0,0 +1,14 @@
+import Resource from "../resource";
+
+export default class User extends Resource {
+  public static get type() {
+    return "user";
+  }
+
+  public static schema = {
+    attributes: {
+      username: String
+    },
+    relationships: {}
+  };
+}
