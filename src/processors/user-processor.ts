0ad7357e0394a7dd3b090e336024efd5828a4dc0
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-05-04 16:53:07 +0200

    implemented user:add operation

--- src/processors/user-processor.ts
+++ src/processors/user-processor.ts
@@ -2,14 +2,41 @@ import Authorize from "../decorators/authorize";
 import jsonApiErrors from "../json-api-errors";
 import { Operation, HasId } from "../types";
 import User from "../resources/user";
-
 import KnexProcessor from "./knex-processor";
+import Password from "../attribute-types/password";
+import { randomBytes } from "crypto";
 
 export default class UserProcessor<T extends User> extends KnexProcessor<T> {
   public static resourceClass = User;
 
   async identify(op: Operation): Promise<HasId[]> {
     return super.get({ ...op, params: {} });
+  }
+
+  async add(op: Operation): Promise<HasId> {
+    const fields = Object.keys(op.data.attributes)
+      .filter(
+        attribute =>
+          this.resourceClass.schema.attributes[attribute] !== Password
+      )
+      .map(attribute => ({ [attribute]: op.data.attributes[attribute] }))
+      .reduce((attributes, attribute) => ({ ...attributes, ...attribute }), {});
+
+    const id = Date.now();
+
+    const encryptedPassword = await this.appInstance.app.services.password(op);
+
+    await this.knex("users").insert({
+      ...fields,
+      ...encryptedPassword,
+      id
+    });
+
+    const user = await this.knex("users")
+      .where({ id })
+      .first();
+
+    return user;
   }
 
   @Authorize()
