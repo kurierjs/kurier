11e87c85ab4267dd5210934e09adfd664873e1f6
Ryan Tablada <ryan.tablada@gmail.com>
2019-05-06 14:21:40 -0500

    feat: start adding jest with basic new test

---
+++ tests/application.test.ts
@@ -0,0 +1,14 @@
+import { Application, KnexProcessor } from "../src";
+
+describe("Application", () => {
+  it('can be instantiated', async () => {
+    const app = new Application({
+      namespace: "api",
+      types: [],
+      processors: [],
+      defaultProcessor: KnexProcessor
+    });
+
+    expect(app).not.toBeNull();
+  });
+});
