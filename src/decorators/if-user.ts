778b449970fac5673e24dd4712fb94d74b19af9c
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-05-22 17:39:16 -0300

    implemented comparers and new helpers

--- src/decorators/if-user.ts
+++ src/decorators/if-user.ts
@@ -1,3 +1,19 @@
-export default function ifUser(attribute: string, value: string | number | boolean | string[] | number[]) {
+import { AttributeValueMatch } from "../types";
+
+export function ifUser(attribute: string, value: string | number | boolean | string[] | number[]): AttributeValueMatch {
   return { attribute, value };
+}
+
+export function ifUserNotMatches(
+  attribute: string,
+  value: string | number | boolean | string[] | number[]
+): AttributeValueMatch {
+  return { attribute, value, operator: "not" };
+}
+
+export function ifUserMatchEvery(
+  attribute: string,
+  value: string | number | boolean | string[] | number[]
+): AttributeValueMatch {
+  return { attribute, value, operator: "every" };
 }
