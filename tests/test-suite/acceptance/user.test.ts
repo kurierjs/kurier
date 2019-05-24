046b687139dd605f32f21103b635c1d37b9229aa
Santiago Persico <santiagopersico@gmail.com>
2019-05-24 12:12:51 -0300

    removes silent failing of errors in tests

--- tests/test-suite/acceptance/user.test.ts
+++ tests/test-suite/acceptance/user.test.ts
@@ -10,53 +10,44 @@ const request = agent(http) as SuperTest<Test>;
 describe("Users", () => {
   describe("POST", () => {
     it("Create user", async () => {
-      try {
-        const result = await request.post("/users").send(users.forCreation.initial)
-        expect(result.status).toEqual(201);
-        expect(result.body).toEqual(users.forCreation.final);
-      } catch (error) { }
+      const result = await request.post("/users").send(users.forCreation.initial)
+      expect(result.status).toEqual(201);
+      expect(result.body).toEqual(users.forCreation.final);
     });
   });
 
   describe("PATCH", () => {
     it("Update a user", async () => {
-      try {
-        const [insertedId] = await context.transaction.table("users").insert(users.toUpdate.initial.data.attributes);
-        const result = await request.patch(`/users/${insertedId}`).send(users.toUpdate.dataToUpdate)
-        expect(result.body).toEqual(users.toUpdate.final);
-        expect(result.status).toEqual(200);
-      } catch (error) { }
+      const [insertedId] = await context.transaction.table("users").insert(users.toUpdate.initial.data.attributes);
+      const result = await request.patch(`/users/${insertedId}`).send(users.toUpdate.dataToUpdate)
+      expect(result.body).toEqual(users.toUpdate.final);
+      expect(result.status).toEqual(200);
     });
   });
 
   describe("GET", () => {
     it("Session Creation", async () => {
-      try {
-        await request.post("/users").send(users.toAuthenticate.user);
-        const result = await request.post("/users").send(users.toAuthenticate.initial);
-
-        expect(result.status).toEqual(200);
-        expect(result.body.data.type).toEqual("session");
-        expect(result.body.data.relationships).toEqual(users.toAuthenticate.final.data.relationships);
-      } catch (error) { }
+      await request.post("/users").send(users.toAuthenticate.user);
+      const result = await request.post("/session").send(users.toAuthenticate.initial);
+      expect(result.status).toEqual(201);
+      expect(result.body.data.id).toBeDefined();
+      expect(result.body.data.attributes.token).toBeDefined();
+      expect(result.body.data.type).toEqual("session");
+      expect(result.body.data.relationships).toEqual(users.toAuthenticate.final.data.relationships);
     });
 
     it("Authenticated - Get all users", async () => {
-      try {
-        const authData = await getAuthenticationData();
-        const result = await request.get("/users").set("Authorization", authData.token);
-        expect(result.status).toEqual(200);
-        expect(result.body).toEqual({ data: users.toGet[0] });
-      } catch (error) { }
+      const authData = await getAuthenticationData();
+      const result = await request.get("/users").set("Authorization", authData.token);
+      expect(result.status).toEqual(200);
+      expect(result.body).toEqual({ data: users.toGet });
     });
 
     it("Authenticated - Gets current user by id", async () => {
-      try {
-        const authData = await getAuthenticationData();
-        const result = await request.get(`/users/${authData.user.id}`).set("Authorization", authData.token);
-        expect(result.status).toEqual(200);
-        expect(result.body).toEqual({ data: users.toGet[0] });
-      } catch (error) { }
+      const authData = await getAuthenticationData();
+      const result = await request.get(`/users/${authData.user.id}`).set("Authorization", authData.token);
+      expect(result.status).toEqual(200);
+      expect(result.body).toEqual({ data: users.toGet[0] });
     });
 
   });
