00e3204b8fb489021a1a96728935f2e30dde9492
Santiago Persico <santiagopersico@gmail.com>
2019-05-28 02:09:29 -0300

    adds new tests, adds more data on factories

--- tests/test-suite/acceptance/article.test.ts
+++ tests/test-suite/acceptance/article.test.ts
@@ -2,6 +2,7 @@ import { SuperTest, Test } from "supertest";
 import * as agent from "supertest-koa-agent";
 import articles from "./factories/article";
 import http from "../test-app/http";
+import getAuthenticationData from "./helpers/authenticateUser";
 
 const request = agent(http) as SuperTest<Test>;
 
@@ -17,6 +18,37 @@ describe("Articles", () => {
       const result = await request.get(`/articles/1`);
       expect(result.status).toEqual(200);
       expect(result.body).toEqual({ data: articles.toGet.response[0] });
+    });
+
+    it("Search Article by %like - Should return all Articles", async () => {
+      const result = await request.get("/articles?filter[body]=like:%test%");
+      expect(result.status).toEqual(200);
+      expect(result.body).toEqual({ data: articles.toGet.response });
+    });
+
+    it("Search Article by %like - Should return empty", async () => {
+      const result = await request.get("/articles?filter[body]=like:this%|like:%this");
+      expect(result.status).toEqual(200);
+      expect(result.body).toEqual({ data: [] });
+    });
+
+    it("Search Article by %like - Should return the first Article", async () => {
+      const result = await request.get("/articles?filter[body]=like:%1");
+      expect(result.status).toEqual(200);
+      expect(result.body).toEqual({ data: [articles.toGet.response[0]] });
+    });
+
+    it("Authenticated - Get an specific article with it's votes and author - Multiple types include", async () => {
+      const authData = await getAuthenticationData();
+      const result = await request.get("/articles/1?include=author,votes").set("Authorization", authData.token);
+      expect(result.status).toEqual(200);
+      expect(result.body).toEqual(articles.singleArticleMultipleIncludes);
+    });
+
+    // TODO: THIS SHOULD'NT WORK WELL - EITHER 401 or simply don't show the author
+    it("UNAuthenticated - Get an specific article with it's author - Should fail", async () => {
+      const result = await request.get("/articles/1?include=author");
+      expect(result.status).toEqual(401);
     });
   });
 });
---
+++ tests/test-suite/acceptance/comment.test.ts
@@ -0,0 +1,29 @@
+import { SuperTest, Test } from "supertest";
+import * as agent from "supertest-koa-agent";
+import comments from "./factories/comment";
+import http from "../test-app/http";
+
+const request = agent(http) as SuperTest<Test>;
+
+describe("Comments", () => {
+  describe("GET", () => {
+    it("Get Comment by id - Only show the body attribute", async () => {
+      const result = await request.get("/comments/1?fields=body");
+      expect(result.status).toEqual(200);
+      expect(result.body).toEqual(comments.singleArticleNoTypeField);
+    });
+
+    it("Get Comments - Test sorted by field request", async () => {
+      const result = await request.get("/comments?sort=-body");
+      expect(result.status).toEqual(200);
+      expect(result.body).toEqual(comments.toGetReverseSorted);
+
+    });
+
+    it("Get Comments - Test pagination and sorting - Should only show one ", async () => {
+      const result = await request.get("/comments?page[number]=0&page[size]=1&sort=-body");
+      expect(result.status).toEqual(200);
+      expect(result.body).toEqual({ data: [comments.toGetReverseSorted.data[2]] });
+    });
+  });
+});
