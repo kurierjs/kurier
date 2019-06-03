dfd7a606cde62dc77c0016144c7b8bcc091ee8f3
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-06-03 14:00:07 -0300

    decomposed decorator, added canAccessResource

--- src/decorators/authorize.ts
+++ src/decorators/authorize.ts
@@ -3,61 +3,72 @@ import { AttributeValueMatch, AttributeValue } from "../types";
 
 import decorateWith from "./decorator";
 import OperationProcessor from "../processors/operation-processor";
-import { Resource } from "..";
+import Resource from "../resource";
+import ApplicationInstance from "../application-instance";
 
 type PrimitiveValue = string | number | boolean | object;
 
 const match = (actual: AttributeValue) => (item: PrimitiveValue) => (actual as any[]).includes(item);
+const ACCESS_RULES = Symbol("accessRules");
+
+function conditionsPass(
+  appInstance: ApplicationInstance,
+  { attribute, value, operator = "some" }: AttributeValueMatch
+) {
+  const actual: AttributeValue = appInstance.user.attributes[attribute];
+  const expected = value;
+
+  if (Array.isArray(actual)) {
+    if (Array.isArray(expected)) {
+      if (operator === "some") {
+        return expected.some(match(actual));
+      }
+
+      if (operator === "every") {
+        return expected.every(match(actual));
+      }
+
+      if (operator === "not") {
+        return !expected.every(match(actual));
+      }
+    }
+
+    if (operator === "not") {
+      return !match(actual)(expected);
+    }
+
+    return match(actual)(expected);
+  }
+
+  if (Array.isArray(expected)) {
+    return match(expected)(actual);
+  }
+
+  if (operator === "not") {
+    return actual !== expected;
+  }
+
+  return actual === expected;
+}
 
 function authorizeMiddleware(operation: Function, conditions: AttributeValueMatch[]) {
-  return function(this: OperationProcessor<Resource>) {
+  const callback = function(this: OperationProcessor<Resource>) {
+    this.constructor["accessRules"][operation.name] = conditions;
+
     if (!this.appInstance.user) {
       throw JsonApiErrors.Unauthorized();
     }
 
-    if (
-      !conditions.every(({ attribute, value, operator = "some" }: AttributeValueMatch) => {
-        const actual: AttributeValue = this.appInstance.user.attributes[attribute];
-        const expected = value;
-
-        if (Array.isArray(actual)) {
-          if (Array.isArray(expected)) {
-            if (operator === "some") {
-              return expected.some(match(actual));
-            }
-
-            if (operator === "every") {
-              return expected.every(match(actual));
-            }
-
-            if (operator === "not") {
-              return !expected.every(match(actual));
-            }
-          }
-
-          if (operator === "not") {
-            return !match(actual)(expected);
-          }
-
-          return match(actual)(expected);
-        }
-
-        if (Array.isArray(expected)) {
-          return match(expected)(actual);
-        }
-
-        if (operator === "not") {
-          return actual !== expected;
-        }
-
-        return actual === expected;
-      })
-    ) {
+    if (!conditions.every((condition: AttributeValueMatch) => conditionsPass(this.appInstance, condition))) {
       throw JsonApiErrors.AccessDenied();
     }
 
     return operation.call(this, ...arguments);
   };
+
+  callback[ACCESS_RULES] = conditions;
+
+  return callback;
 }
 
 /**
@@ -67,4 +78,15 @@ function authorizeMiddleware(operation: Function, conditions: AttributeValueMatc
  */
 export default function authorize(...conditions: AttributeValueMatch[]) {
   return decorateWith(authorizeMiddleware, conditions);
+}
+
+export async function canAccessResource(resource: Resource, operationName: string, appInstance: ApplicationInstance) {
+  const processor = await appInstance.processorFor(resource.type);
+  const accessRules = processor[operationName][ACCESS_RULES] || [];
+
+  if (!accessRules.length) {
+    return true;
+  }
+
+  return accessRules.every((condition: AttributeValueMatch) => conditionsPass(appInstance, condition));
 }
