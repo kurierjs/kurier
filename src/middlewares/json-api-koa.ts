0b1c11a026870cb41ba5930e502068f04db839f4
Santiago Persico <santiagopersico@gmail.com>
2019-06-05 19:57:45 -0300

    refactor koa middleware, using common functions

--- src/middlewares/json-api-koa.ts
+++ src/middlewares/json-api-koa.ts
@@ -1,141 +1,41 @@
-import * as escapeStringRegexp from "escape-string-regexp";
 import { Context, Middleware } from "koa";
 import * as koaBody from "koa-body";
 import * as compose from "koa-compose";
 
 import Application from "../application";
-import JsonApiErrors from "../errors/json-api-errors";
-import { JsonApiDocument, JsonApiErrorsDocument, Operation, OperationResponse } from "../types";
-import { parse } from "../utils/json-api-params";
-import { camelize, singularize } from "../utils/string";
 import ApplicationInstance from "../application-instance";
-import User from "../resources/user";
-import JsonApiError from "../errors/error";
-
-const STATUS_MAPPING = {
-  GET: 200,
-  POST: 201,
-  PATCH: 200,
-  PUT: 200,
-  DELETE: 204
-};
+import {
+  authenticate,
+  urlData,
+  handleBulkEndpoint,
+  handleJsonApiEndpoint,
+  convertErrorToHttpResponse
+} from "../utils/http-utils";
 
 export default function jsonApiKoa(app: Application, ...middlewares: Middleware[]) {
   const jsonApiKoa = async (ctx: Context, next: () => Promise<any>) => {
     const appInstance = new ApplicationInstance(app);
 
     try {
-      await authenticate(appInstance, ctx);
+      await authenticate(appInstance, ctx.request);
     } catch (error) {
       ctx.body = convertErrorToHttpResponse(error);
       ctx.status = error.status;
       return next();
     }
 
-    const data = urlData(appInstance, ctx);
+    ctx.request["urlData"] = urlData(appInstance, ctx.path);
 
-    if (ctx.method === "PATCH" && data.resource === "bulk") {
-      await handleBulkEndpoint(appInstance, ctx);
+    if (ctx.method === "PATCH" && ctx.request["urlData"].resource === "bulk") {
+      ctx.body = await handleBulkEndpoint(appInstance, ctx.request.body.operations);
       return next();
     }
 
-    ctx.urlData = data;
-    return handleJsonApiEndpoint(appInstance, ctx).then(() => next());
+    const { body, status } = await handleJsonApiEndpoint(appInstance, ctx.request);
+    ctx.body = body;
+    ctx.status = status;
+    return next();
   };
 
   return compose([koaBody({ json: true }), ...middlewares, jsonApiKoa]);
-}
-
-async function authenticate(appInstance: ApplicationInstance, ctx: Context) {
-  const authHeader = ctx.request.headers.authorization;
-  let currentUser: User;
-
-  if (authHeader && authHeader.startsWith("Bearer ")) {
-    const [, token] = authHeader.split(" ");
-    currentUser = await appInstance.getUserFromToken(token);
-  }
-
-  appInstance.user = currentUser;
-}
-
-function urlData(appInstance: ApplicationInstance, ctx: Context) {
-  const urlRegexp = new RegExp(
-    `^(\/+)?((?<namespace>${escapeStringRegexp(
-      appInstance.app.namespace
-    )})(\/+|$))?(?<resource>[^\\s\/?]+)?(\/+)?((?<id>[^\\s\/?]+)?(\/+)?\(?<relationships>relationships)?(\/+)?)?` +
-    "(?<relationship>[^\\s/?]+)?(/+)?$"
-  );
-
-  const { resource, id, relationships, relationship } = (ctx.path.match(urlRegexp) || {})["groups"] || ({} as any);
-
-  return {
-    id,
-    resource,
-    relationship,
-    isRelationships: !!relationships
-  };
-}
-
-async function handleBulkEndpoint(appInstance: ApplicationInstance, ctx: Context) {
-  const operations = await appInstance.app.executeOperations(ctx.request.body.operations || []);
-
-  ctx.body = { operations };
-}
-
-async function handleJsonApiEndpoint(appInstance: ApplicationInstance, ctx: Context) {
-  const op: Operation = convertHttpRequestToOperation(ctx);
-  if (["update", "remove"].includes(op.op) && !op.ref.id) return;
-
-  try {
-    const [result]: OperationResponse[] = await appInstance.app.executeOperations([op], appInstance);
-
-    ctx.body = convertOperationResponseToHttpResponse(ctx, result);
-    ctx.status = STATUS_MAPPING[ctx.method];
-  } catch (error) {
-    ctx.body = convertErrorToHttpResponse(error);
-    ctx.status = error.status || 500;
-  }
-}
-
-function convertHttpRequestToOperation(ctx: Context): Operation {
-  const { id, resource, relationship } = ctx.urlData;
-  const type = camelize(singularize(resource));
-
-  const opMap = {
-    GET: "get",
-    POST: "add",
-    PATCH: "update",
-    PUT: "update",
-    DELETE: "remove"
-  };
-
-  return {
-    op: opMap[ctx.method],
-    params: parse(ctx.href),
-    ref: { id, type, relationship },
-    data: ctx.request.body.data
-  } as Operation;
-}
-
-function convertOperationResponseToHttpResponse(ctx: Context, operation: OperationResponse): JsonApiDocument {
-  const responseMethods = ["GET", "POST", "PATCH", "PUT"];
-
-  if (responseMethods.includes(ctx.method)) {
-    return { data: operation.data, included: operation.included };
-  }
-}
-
-function convertErrorToHttpResponse(error: JsonApiError): JsonApiErrorsDocument {
-  const isJsonApiError = error && error.status;
-  if (!isJsonApiError) console.error("JSONAPI-TS: ", error);
-
-  const jsonApiError = isJsonApiError ? error : JsonApiErrors.UnhandledError();
-  if ((!process.env.NODE_ENV || process.env.NODE_ENV !== "production") && error.stack && !isJsonApiError) {
-    let firstLineErrorStack = error.stack.split("\n")[0];
-    if (firstLineErrorStack.indexOf('Error:') === 0) {
-      firstLineErrorStack = firstLineErrorStack.slice(7);
-    }
-    jsonApiError.detail = firstLineErrorStack;
-  }
-  return { errors: [jsonApiError] };
 }
