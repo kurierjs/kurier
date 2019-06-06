d36381f2bb9f25a6870de09273bfd1855b5e27a3
Santiago Persico <santiagopersico@gmail.com>
2019-06-06 16:24:57 -0300

    simplify  config

--- jest.config.js
+++ jest.config.js
@@ -1,35 +1,26 @@
-const baseProject = {
-  moduleNameMapper: {
-    "^@ebryn/jsonapi-ts": "<rootDir>/src",
-    "^@ebryn/jsonapi-ts/(.*)$": "<rootDir>/src/$1",
-    "^@acceptance/(.*)$": "<rootDir>/tests/acceptance/$1",
-    "^@dummy/(.*)$": "<rootDir>/tests/dummy/src/$1",
-    "^@unit/(.*)$": "<rootDir>/tests/unit/$1"
-  },
-  moduleFileExtensions: ["ts", "js"],
-  transform: {
-    "^.+\\.(ts)$": "ts-jest"
-  },
-  globals: {
-    "ts-jest": {
-      tsConfig: "tsconfig.test.json"
-    }
-  },
-  testMatch: ["<rootDir>/tests/test-suite/acceptance/**/?(*.)+(spec|test).ts"]
-};
-
 module.exports = {
   globalSetup: "<rootDir>/tests/test-suite/globalSetup.ts",
   moduleFileExtensions: ["ts", "js"],
   reporters: ["default", "jest-junit"],
   projects: [
     {
-      ...baseProject,
+      moduleFileExtensions: ["ts", "js"],
+      transform: {
+        "^.+\\.(ts)$": "ts-jest"
+      },
       displayName: "unit",
+      globals: {
+        "ts-jest": {
+          tsConfig: "tsconfig.test.json"
+        }
+      },
       testMatch: ["<rootDir>/tests/test-suite/unit/**/?(*.)+(spec|test).ts"]
     },
     {
-      ...baseProject,
+      moduleFileExtensions: ["ts", "js"],
+      transform: {
+        "^.+\\.(ts)$": "ts-jest"
+      },
       displayName: "acceptance - camelCase",
       globals: {
         TEST_SUITE: "test_camelCase",
@@ -37,10 +28,14 @@ module.exports = {
           tsConfig: "tsconfig.test.json"
         }
       },
+      testMatch: ["<rootDir>/tests/test-suite/acceptance/**/?(*.)+(spec|test).ts"],
       setupFilesAfterEnv: ["<rootDir>/tests/test-suite/setup.ts"]
     },
     {
-      ...baseProject,
+      moduleFileExtensions: ["ts", "js"],
+      transform: {
+        "^.+\\.(ts)$": "ts-jest"
+      },
       displayName: "acceptance - snake_case",
       globals: {
         TEST_SUITE: "test_snake_case",
@@ -48,6 +43,7 @@ module.exports = {
           tsConfig: "tsconfig.test.json"
         }
       },
+      testMatch: ["<rootDir>/tests/test-suite/acceptance/**/?(*.)+(spec|test).ts"],
       setupFilesAfterEnv: ["<rootDir>/tests/test-suite/setup.ts"]
     }
   ]
