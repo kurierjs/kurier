b6e31e484f993c4b78bb087fc418855dee6ca5a9
Santiago Persico <santiagopersico@gmail.com>
2019-06-06 15:56:52 -0300

    stuff missing

--- tests/test-suite/setup.ts
+++ tests/test-suite/setup.ts
@@ -3,30 +3,35 @@ import app from "./test-app/app";
 import context from "./transaction";
 import knexfile from "../data/knexfile";
 import Serializer from "./test-app/serializers/serializer";
-let knexInstance;
+let knexInstance = Knex(knexfile[global["TEST_SUITE"]]);
 const createTransaction = (connection, callback): Promise<Knex.Transaction> => {
-  return new Promise(resolve =>
+  return new Promise(resolve => {
     connection
       .transaction(t => {
         callback(t);
         return resolve(t);
       })
-      .catch(t => { })
-  );
+      .catch(t => { });
+  });
 };
 const serializer = global["TEST_SUITE"] === "test_snake_case" ? app.serializer : new Serializer();
 
 beforeAll(async () => {
-  knexInstance = await createTransaction(Knex(knexfile[global["TEST_SUITE"]]), () => { });
   app.serializer = serializer;
 });
 
 beforeEach(async () => {
   context.transaction = await createTransaction(knexInstance, transaction => {
+    transaction.initialize();
     app.services.knex = transaction;
   });
 });
 
 afterEach(async () => {
   await context.transaction.rollback();
+  await context.transaction.destroy();
+});
+
+afterAll(async () => {
+  await knexInstance.destroy();
 });
