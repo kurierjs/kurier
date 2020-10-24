df44ed7b903c3935d7b5600c3622295994ed2c40
Santiago Persico <santiagopersico@gmail.com>
2020-10-24 19:43:48 -0300

    add nested resources tests

--- tests/test-suite/acceptance/nestedResources.test.ts
+++ tests/test-suite/acceptance/nestedResources.test.ts
@@ -7,11 +7,36 @@ const request = testTransportLayer("koa");
 describe("Nested Resources", () => {
 
   describe("GET", () => {
-    it("Get 1st article'Author and his Votes (hasMany)(Multiple Results)", async () => {
+    it("Get the 1st article'Author and his Votes (*-1-*)", async () => {
       const authData = await getAuthenticationData();
       const result = await request.get("/articles/1?include=author.votes")
         .set("Authorization", authData.token);
       expect(result.status).toEqual(200);
+      expect(result.body).toEqual(nested.get.authorAndAuthorVotesOf1stArticle);
+    });
+
+    it("Get the 2nd User's articles and their Votes (1-*-*)", async () => {
+      const authData = await getAuthenticationData();
+      const result = await request.get("/users/2?include=articles.votes")
+        .set("Authorization", authData.token);
+      expect(result.status).toEqual(200);
+      expect(result.body).toEqual(nested.get.articlesAndArticlesVotsOf2ndUser);
+    });
+
+    it("Get the 1nd Comment parent's Comment's author (1-1-*)", async () => {
+      const authData = await getAuthenticationData();
+      const result = await request.get("/comment/1?include=parentComment.author")
+        .set("Authorization", authData.token);
+      expect(result.status).toEqual(200);
+      expect(result.body).toEqual(nested.get.parentCommentAndParentCommentsAuthorOf1stComment);
+    });
+
+    it("Get the 1nd Comment's parent's comment parent's comment (1-1-1)", async () => {
+      const authData = await getAuthenticationData();
+      const result = await request.get("/comment/1?include=parentComment.parentComment")
+        .set("Authorization", authData.token);
+      expect(result.status).toEqual(200);
+      expect(result.body).toEqual(nested.get.parentCommentAndParentCommentsParentCommentOf1stComment);
     });
   });
 
