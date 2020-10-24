64962e0b69df4429e44494dab7f3f71b3675190d
Santiago Persico <santiagopersico@gmail.com>
2020-10-24 19:43:34 -0300

    add extra relationship test utilty

--- tests/test-suite/acceptance/factories/utils.ts
+++ tests/test-suite/acceptance/factories/utils.ts
@@ -1,4 +1,4 @@
-import { Resource } from "../../../../src"
+import { Resource, ResourceRelationships } from "../../../../src"
 
 export const getFactoryObject =
   (array: any[], key: string = 'id') => (id: string | number): Resource =>
@@ -7,3 +7,18 @@ export const getFactoryObject =
 export const getFactoryObjects =
   (array: any[], key: string = 'id') => (ids: (string | number)[]): Resource[] =>
     array.filter(object => ids.includes(object[key]))
+
+    export const getExtraRelationships =
+  (array: any[], relationshipName?:string, key: string = 'id',) =>
+  (ids: (string | number)[], format:'Array'|'Object' ='Array'): ResourceRelationships => {
+    const relationship = format==='Object'? {
+      id:getFactoryObject(array, key)(ids[0]).id,
+      type: getFactoryObject(array, key)(ids[0]).type
+    } : getFactoryObjects(array, key)(ids).map(({id,type})=>({id,type}))
+
+     return {
+       [relationshipName]: {
+         data: relationship
+      }
+    }
+  }
