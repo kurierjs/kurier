ee60de6ecdfd88b388678f63a495c591f589552c
Ivica Batinic <ivica.batinic@infinum.hr>
2021-06-07 15:59:04 +0200

    Extract favicon middleware to function

--- tests/dummy-app/http.ts
+++ tests/dummy-app/http.ts
@@ -3,10 +3,21 @@ import * as Koa from "koa";
 import * as express from "express";
 import { jsonApiKoa, jsonApiExpress } from "./kurier";
 
+const faviconMiddleware = async (req, res, next) => {
+  if (req.url === '/favicon.ico') {
+    res.statusCode = 404;
+    res.end();
+  } else {
+    await next();
+  }
+};
+
 const koaApp = new Koa();
+koaApp.use(async (ctx, next) => faviconMiddleware(ctx.req, ctx.res, next));
 koaApp.use(jsonApiKoa(app));
 
 const expressApp = express();
+expressApp.use(faviconMiddleware);
 expressApp.use(jsonApiExpress(app));
 
 export { expressApp, koaApp };
--- tests/test-suite/test-app/http.ts
+++ tests/test-suite/test-app/http.ts
@@ -3,26 +3,21 @@ import * as Koa from "koa";
 import * as express from "express";
 import { jsonApiKoa, jsonApiExpress } from "./kurier";
 
-const koaApp = new Koa();
-koaApp.use(async (ctx, next) => {
-  if (ctx.req.url === '/favicon.ico') {
-    ctx.res.statusCode = 404;
-    ctx.res.end();
-  } else {
-    await next();
-  }
-});
-koaApp.use(jsonApiKoa(app));
-
-const expressApp = express();
-expressApp.use(async (req, res, next) => {
+const faviconMiddleware = async (req, res, next) => {
   if (req.url === '/favicon.ico') {
     res.statusCode = 404;
     res.end();
   } else {
     await next();
   }
-});
+};
+
+const koaApp = new Koa();
+koaApp.use(async (ctx, next) => faviconMiddleware(ctx.req, ctx.res, next));
+koaApp.use(jsonApiKoa(app));
+
+const expressApp = express();
+expressApp.use(faviconMiddleware);
 expressApp.use(jsonApiExpress(app));
 
 export { expressApp, koaApp };
