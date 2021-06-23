637f75e8de7b522c3844e6beab3184a22b3d04c5
Joel A. Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2021-06-23 20:09:12 -0300

    populating urlData from req.query directly

--- src/middlewares/json-api-vercel.ts
+++ src/middlewares/json-api-vercel.ts
@@ -15,6 +15,10 @@ export default function jsonApiVercel(app: Application) {
   return async (req: VercelRequest, res: VercelResponse) => {
     const appInstance = new ApplicationInstance(app);
 
+    if (['POST', 'PUT', 'PATCH'].includes(req.method!) && typeof req.body === 'string') {
+      req.body = JSON.parse(req.body);
+    }
+
     try {
       await authenticate(appInstance, req);
     } catch (error) {
@@ -27,16 +31,21 @@ export default function jsonApiVercel(app: Application) {
       req["href"] = `https://${req.headers.host}${req.url!}`;
     } else {
       // This means we're in development. We won't use HTTPS here.
-      req["href"] = `http://localhost${req.url!}`;
+      req["href"] = `http://${req.headers.host}${req.url!}`;
     }
 
-    req["urlData"] = urlData(appInstance, req.url!);
+    req["urlData"] = {
+      id: req.query.kurier[1],
+      resource: req.query.kurier[0],
+    }
 
     if (req.method === "PATCH" && req["urlData"].resource === "bulk") {
       const bulkResponse = await handleBulkEndpoint(appInstance, req.body.operations as Operation[]);
       res.json(bulkResponse);
       return;
     }
+
+    console.log(req.query);
 
     const { body, status } = await handleJsonApiEndpoint(appInstance, req);
     res.status(status)
