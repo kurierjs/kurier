7aad9773b6f8e04126f09250f086e23206f896e1
Joel A. Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2021-06-23 21:28:52 -0300

    tests now run against each HTTP transport layer

--- tests/test-suite/acceptance/session.test.ts
+++ tests/test-suite/acceptance/session.test.ts
@@ -1,24 +1,26 @@
 import users from "./factories/user";
-import testTransportLayer from "./helpers/transportLayers";
+import testTransportLayer, {transportLayers} from "./helpers/transportLayers";
 
-const request = testTransportLayer("koa");
+describe.each(transportLayers)("Transport Layer: %s", (transportLayer) => {
+  const request = testTransportLayer(transportLayer);
 
-describe("Session", () => {
-  it("POST", async () => {
-    const sessionResponse = await request.post("/session").send(users.toAuthenticate.request);
-    expect(sessionResponse.status).toEqual(201);
-    expect(sessionResponse.body.data.id).toBeDefined();
-    expect(sessionResponse.body.data.attributes.token).toBeDefined();
-    expect(sessionResponse.body.data.type).toEqual("session");
-    expect(sessionResponse.body.data.relationships).toEqual(users.toAuthenticate.response.data.relationships);
-  });
+  describe("Session", () => {
+    it("POST", async () => {
+      const sessionResponse = await request.post("/session").send(users.toAuthenticate.request);
+      expect(sessionResponse.status).toEqual(201);
+      expect(sessionResponse.body.data.id).toBeDefined();
+      expect(sessionResponse.body.data.attributes.token).toBeDefined();
+      expect(sessionResponse.body.data.type).toEqual("session");
+      expect(sessionResponse.body.data.relationships).toEqual(users.toAuthenticate.response.data.relationships);
+    });
 
-  it("Login - Complete flow", async () => {
-    const sessionResponse = await request.post("/session").send(users.toAuthenticate.request);
-    const userId = sessionResponse.body.data.relationships.user.data.id;
-    const token = `Bearer ${sessionResponse.body.data.attributes.token}`;
-    const userResponse = await request.get(`/users/${userId}`).set("Authorization", token);
-    expect(userResponse.status).toEqual(200);
-    expect(userResponse.body).toEqual({ data: users.toGet[0] });
+    it("Login - Complete flow", async () => {
+      const sessionResponse = await request.post("/session").send(users.toAuthenticate.request);
+      const userId = sessionResponse.body.data.relationships.user.data.id;
+      const token = `Bearer ${sessionResponse.body.data.attributes.token}`;
+      const userResponse = await request.get(`/users/${userId}`).set("Authorization", token);
+      expect(userResponse.status).toEqual(200);
+      expect(userResponse.body).toEqual({ data: users.toGet[0] });
+    });
   });
 });
--- tests/test-suite/acceptance/vote.test.ts
+++ tests/test-suite/acceptance/vote.test.ts
@@ -1,16 +1,17 @@
 import vote from "./factories/vote";
 import getAuthenticationData from "./helpers/authenticateUser";
-import testTransportLayer from "./helpers/transportLayers";
+import testTransportLayer, {transportLayers} from "./helpers/transportLayers";
 
-const request = testTransportLayer("koa");
-
-describe("Votes", () => {
-  describe("GET", () => {
-    it("Gets vote by id", async () => {
-      const { token } = await getAuthenticationData();
-      const result = await request.get(`/votes/1?include=user,article`).set("Authorization", token);
-      expect(result.status).toEqual(200);
-      expect(result.body).toEqual(vote.toGetIncludedUserAndArticle);
+describe.each(transportLayers)("Transport Layer: %s", (transportLayer) => {
+  const request = testTransportLayer(transportLayer);
+  describe("Votes", () => {
+    describe("GET", () => {
+      it("Gets vote by id", async () => {
+        const { token } = await getAuthenticationData();
+        const result = await request.get(`/votes/1?include=user,article`).set("Authorization", token);
+        expect(result.status).toEqual(200);
+        expect(result.body).toEqual(vote.toGetIncludedUserAndArticle);
+      });
     });
   });
 });
