87d717b6b5f77fabe7b80cec609c8bffd434f702
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2023-01-18 14:35:34 -0300

    feat(hooks): implementation & types

---
+++ src/utils/hooks.ts
@@ -0,0 +1,16 @@
+import jsonApiErrors from "../errors/json-api-errors";
+import { ApplicationHooks, ApplicationInstanceInterface } from "../types";
+
+export const runHookFunctions = async (
+  appInstance: ApplicationInstanceInterface,
+  hookType: keyof ApplicationHooks,
+  parameters: Record<string, any> = {},
+) => {
+  for (const hook of appInstance.app.hooks[hookType]) {
+    try {
+      await hook(appInstance, parameters);
+    } catch (e) {
+      throw jsonApiErrors.UnhandledError(`Hook ${hookType}:${hook.name} failed: ${e.message}, ${e.stack}`);
+    }
+  }
+};
